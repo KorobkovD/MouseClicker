67560e5eb82e0e7dc6a0c424afdf5eb97599ec25
KorobkovD <den-kor.int@mail.ru>
2023-09-30 13:32:40 +0300

    Rename project

--- MouseClicker/Properties/AssemblyInfo.cs
+++ MouseClicker/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("MouseForms")]
+[assembly: AssemblyTitle("MouseClicker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MouseForms")]
+[assembly: AssemblyProduct("MouseClicker")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
